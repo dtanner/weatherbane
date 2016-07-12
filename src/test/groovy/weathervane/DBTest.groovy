9cccdff2e491e73cd894e500e55c89881f6553dc
dan <dan@dantanner.com>
2016-07-12 00:15:49 -0500

    ok travis, what's your file path?

--- src/test/groovy/weathervane/DBTest.groovy
+++ src/test/groovy/weathervane/DBTest.groovy
@@ -1,14 +1,25 @@
 package weathervane
 
 import groovy.json.JsonSlurper
+import groovy.util.logging.Slf4j
 import spock.lang.Specification
 import weathervane.collector.wunderground.WundergroundCollector
+import weathervane.util.CommandUtil
 
+@Slf4j
 class DBTest extends Specification {
 
     def "store predictions"() {
         given:
-        InputStream sampleFileIS = this.class.classLoader.getResourceAsStream('wunderground-3day-sample.json')
+        String path = this.class.getResource("").path
+        log.info "class.getResource path = $path"
+
+        String classloaderPath = this.class.classLoader.getResource("").path
+        log.info "classloaderPath.getResource path = $classloaderPath"
+
+        CommandUtil.execute('ls -lR')
+
+        InputStream sampleFileIS = this.class.classLoader.getResourceAsStream('./wunderground-3day-sample.json')
         def json = new JsonSlurper().parse(sampleFileIS)
 
         when:
