3f23a5e9b12e2b0759df28a586170c67550ca6e3
dan <dan@dantanner.com>
2016-08-21 14:58:04 -0500

    add some manual queries

--- db/manual-queries.sql
+++ db/manual-queries.sql
@@ -9,3 +9,18 @@ select date, location, high, low, precip
 from observation
 where date = '2016-07-27'
       and location = 'MSP';
+
+
+select location, days_before, target_date, provider, high, low, pop
+from prediction
+where days_before <= 1
+      and location = 'MSP'
+      and target_date = '2016-08-05'
+order by provider, days_before;
+
+select date, location, high, low, precip
+from observation
+where location = 'MSP'
+      and date = '2016-08-05'
+order by date desc
+limit 100;
